00f262840ecb4d6f3be2bc01e1cd584d52dbe695
Potapchuk Anna <potapchuk.anna.kp.01@gmail.com>
2024-03-02 20:01:48 +0200

    colors and lesson module

--- faculty-frontend/src/app/app-routing.module.ts
+++ faculty-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'lessons',
+    canActivate: [AuthGuard],
+     loadChildren: ()=> import('./modules/lesson/lesson.module').then((m)=>m.LessonModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
